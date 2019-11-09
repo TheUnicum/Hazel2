8eb3513db70e1ad000ad7ca0b7ef2d4a11ba0b3e
TheUnicum <tia_ben@tin.it>
2019-11-09 20:25:54 +0100

    Modern OpenGL <Glad>.

--- premake5.lua
+++ premake5.lua
@@ -14,8 +14,10 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 
 include "Hazel/vendor/GLFW"
+include "Hazel/vendor/Glad"
 
 project "Hazel"
     location "Hazel"
@@ -33,20 +35,22 @@ project "Hazel"
         "%{prj.name}/src/**.h",
         "%{prj.name}/src/**.cpp",
     }
-
-    links
-    {
-        "GLFW", 
-        "opengl32.lib",
-    }
-
+    
     includedirs
     {
         "%{prj.name}/src",
         "%{prj.name}/vendor/spdlog/include",
 		IncludeDir.GLFW,
+		IncludeDir.Glad,
     }
-
+    
+    links
+    {
+        "GLFW", 
+        "Glad", 
+        "opengl32.lib",
+    }
+    
     filter "system:windows"
         staticruntime "On"
         systemversion "latest"
@@ -55,6 +59,7 @@ project "Hazel"
         {
             "HZ_PLATFORM_WINDOWS",
             "HZ_BUILD_DLL",
+            "GLFW_INCLUDE_NONE",
         }
 
         postbuildcommands
