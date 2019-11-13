3c5b6dd294e32c60a28be144bb5782e8935302dd
TheUnicum <tia_ben@tin.it>
2019-11-13 18:00:30 +0100

    [34-35] Creating an Orthographic Camera.

---
+++ Hazel/src/Hazel/Renderer/OrthographicCamera.cpp
@@ -0,0 +1,23 @@
+#include "hzpch.h"
+#include "OrthographicCamera.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace Hazel {
+
+	OrthographicCamera::OrthographicCamera(float left, float right, float buttom, float top)
+		:m_ProjectionMatrix(glm::ortho(left, right, buttom, top, -1.0f, +1.0f)), m_ViewMatrix(1.0f)
+	{
+		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+	}
+
+	void OrthographicCamera::RecalculateViewMatrix()
+	{
+		glm::mat4 trasform = glm::translate(glm::mat4(1.0f), m_Position) *
+			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		m_ViewMatrix = glm::inverse(trasform);
+		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+	}
+
+}
