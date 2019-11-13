b98bedd5742dd1444a87ee381a2658be70c3b183
TheUnicum <tia_ben@tin.it>
2019-11-13 11:38:47 +0100

    [32] Vertex Arrays -> Bug [&]missing in Buffer.h!

--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -77,7 +77,7 @@ namespace Hazel {
 		}
 
 		inline uint32_t GetStride() const { return m_Stride; }
-		inline const std::vector<BufferElement> GetElemenst() const { return m_Elements; }
+		inline const std::vector<BufferElement>& GetElemenst() const { return m_Elements; }
 
 		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
 		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
